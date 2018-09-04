4673d2100c4d55f7018efccc4638443957915429
DevFarzan <farzanhanif123@gmail.com>
2018-09-04 18:17:04 +0500

    changing in all files

--- client/src/components/business/Slider.js
+++ client/src/components/business/Slider.js
@@ -12,12 +12,22 @@ class Slider extends Component{
             <div style={{backgroundImage: 'url(https://images.alphacoders.com/633/633643.jpg)',"height": "407px","margin-top": "-19px"}}>
               <p className="paragragh-text">Lorem Forem</p>
               <h1 className="text-h1">Lorem Forem</h1>
-              <form className="navbar-form navbar-left">
-                <div className="form-group2 col-md-8 col-sm-8 col-xs-8">
-                  <input type="text" className="form-control" placeholder="Search"/>
-                </div>
-                <button type="submit" className="btn btn-default col-md-4 col-sm-4 col-xs-4">Submit</button>
-              </form>
+                <section className="home-newsletter">
+                     <div className="container">
+                       <div className="row">
+                         <div className="col-md-12 col-sm-12 col-xs-12">
+                             <div className="single">
+                                <div className="input-group">
+                                <input type="email" className="form-control" placeholder="Enter your email"/>
+                                <span className="input-group-btn">
+                                <button className="btn btn-theme" type="submit">Search</button>
+                                </span>
+                               </div>
+                             </div>
+                         </div>
+                       </div>
+                     </div>
+                 </section>
             </div>
     </div>
   );

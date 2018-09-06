d2be9136f0b34eff96d71daf6ee92e2211859e85
DevFarzan <farzanhanif123@gmail.com>
2018-09-06 14:30:46 +0500

    changing in all

--- models/category.js
+++ models/category.js
@@ -3,8 +3,7 @@ var config = require('../config/config')
 var uniqueValidator = require('mongoose-unique-validator');
 
 var categorySchema = new mongoose.Schema({
-  categoryName:{type:Array, unique: true, required: true,uniqueCaseInsensitive: true},
-  insertedDate:Array,
+  
 });
 categorySchema.plugin(uniqueValidator);
 mongoose.model('category',categorySchema);

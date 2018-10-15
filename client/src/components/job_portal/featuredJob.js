2c3dcef3fbe22f6050024f0b98ccbc2dcbe5741a
Shaayan <shayanmutahir123@yahoo.com>
2018-10-15 05:40:21 -0700

    card modified

--- client/src/components/job_portal/featuredJob.js
+++ client/src/components/job_portal/featuredJob.js
@@ -31,8 +31,78 @@ class FeaturedBox extends Component{
                               Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                           </div>
                           <div className="m-t text-righ">
-                              <a href="#" className="btn btn-xs btn-outline btn-primary">Info <i className="fa fa-long-arrow-right"></i> </a>
-                                <a href="#" className="btn btn-xs btn-outline btn-primary">Info <i className="fa fa-long-arrow-right"></i> </a>
+                              <button type="button" className="btn btn-success">Veiw More</button>
+                            <div className="button2">
+                              <button type="button" className="btn btn-success">Apply Now</button>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="col-md-4">
+              <div className="featuredbox">
+                  <div className="featuredbox-content featuredjob-box">
+                      <div className="featuredjob-imitation">
+                      <div className="card2">
+                        <img alt='' src='./images/job-category.jpeg'/>
+                      </div>
+                      </div>
+                      <div className="customjob-margin">
+                          <h4> IT Person Needed </h4>
+                          <i className="glyphicon glyphicon-star"/>
+                          <p className="textforjob">Full Time</p>
+                        <div className="glyphicom">
+                          <i className="glyphicon glyphicon-map-marker"/>
+                          <p className="textforjob">California</p>
+                        </div>
+                      </div>
+
+                      <div className="jobdetail-desc">
+                          <div> </div>
+                          <div className="small m-t-xs">
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                          </div>
+                          <div className="m-t text-righ">
+                              <button type="button" className="btn btn-success">Veiw More</button>
+                            <div className="button2">
+                              <button type="button" className="btn btn-success">Apply Now</button>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div className="col-md-4">
+              <div className="featuredbox">
+                  <div className="featuredbox-content featuredjob-box">
+                      <div className="featuredjob-imitation">
+                      <div className="card2">
+                        <img alt='' src='./images/job-category.jpeg'/>
+                      </div>
+                      </div>
+                      <div className="customjob-margin">
+                          <h4> IT Person Needed </h4>
+                          <i className="glyphicon glyphicon-star"/>
+                          <p className="textforjob">Full Time</p>
+                        <div className="glyphicom">
+                          <i className="glyphicon glyphicon-map-marker"/>
+                          <p className="textforjob">California</p>
+                        </div>
+                      </div>
+
+                      <div className="jobdetail-desc">
+                          <div> </div>
+                          <div className="small m-t-xs">
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                          </div>
+                          <div className="m-t text-righ">
+                              <button type="button" className="btn btn-success">Veiw More</button>
+                            <div className="button2">
+                              <button type="button" className="btn btn-success">Apply Now</button>
+                            </div>
                           </div>
                       </div>
                   </div>

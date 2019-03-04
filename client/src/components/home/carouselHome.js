5ca20838d88db602c7deba455d11d4a3f8a57606
Siddiq Abbasi <siddiqabbasi0@gmail.com>
2019-03-04 18:36:31 +0500

    carousel work

--- client/src/components/home/carouselHome.js
+++ client/src/components/home/carouselHome.js
@@ -7,6 +7,7 @@ class CarouselHome extends Component{
       super(props)
       this.state = {
         arrayListing:[],
+        // from: 0,
         to:3
       }
   }
@@ -18,19 +19,25 @@ class CarouselHome extends Component{
     }
   }
 
-  handleEvent = (e) =>{
+  handleBackward = (e) =>{
     this.setState({
       to : this.state.to+3
     })
   }
 
 
+    handleFarward = e => {
+      if(this.state.to >= 3){
+        this.setState({
+          to : this.state.to-3
+        })
+      }
+    }
 
 
   render(){
     const { arrayListing,to } = this.state,
     { detail } = this.props;
-    
     return(
       <div className="carousel-reviews broun-block">
         <div className="container" style={{width:"100%"}}>
@@ -38,7 +45,7 @@ class CarouselHome extends Component{
                 <div id="carousel-reviews" className="carousel slide" data-ride="carousel">
                     <div className="carousel-inner">
                     {arrayListing.map((elem,key) => {
-                      if(key<to){
+                      if(key >= to - 3 && key < to){
                       return (
                         <div key={key} className="item active">
                             <div className="col-md-4 col-sm-6">
@@ -53,10 +60,10 @@ class CarouselHome extends Component{
                     }
               </div>
               <a className="left carousel-control" href="#carousel-reviews" role="button" data-slide="prev">
-                  <span className="glyphicon glyphicon-chevron-left" id="left" onClick={e => this.handleEvent(e)}></span>
+                  <span className="glyphicon glyphicon-chevron-left" id="left" onClick={e => this.handleFarward(e)}></span>
               </a>
               <a className="right carousel-control" href="#carousel-reviews" role="button" data-slide="next">
-                  <span className="glyphicon glyphicon-chevron-right" id="right" onClick={e => this.handleEvent(e)}></span>
+                  <span className="glyphicon glyphicon-chevron-right" id="right" onClick={e => this.handleBackward(e)}></span>
               </a>
                 </div>
             </div>

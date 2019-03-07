2e5e422657f54b3df0d9a8269618609a494c36d3
Siddiq Abbasi <siddiqabbasi0@gmail.com>
2019-03-07 17:42:47 +0500

    home cards

--- server.js
+++ server.js
@@ -436,10 +436,11 @@ app.post('/api/forgotPassword', (req, res) => {
                         text:
                           'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n'
                           + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n'
-                          + `${protocol}://${host}/reset/${token}\n\n`
+                          + `http://localhost:3000/reset/${token}\n\n`
                           + 'If you did not request this, please ignore this email and your password will remain unchanged.\n',
                     };
                     // + `http://localhost:3000/reset/${token}\n\n`
+                    // + `${protocol}://${host}/reset/${token}\n\n`
                     
                     smtpTransport.sendMail(mailOptions, (err, response) => {
                       if (err) {

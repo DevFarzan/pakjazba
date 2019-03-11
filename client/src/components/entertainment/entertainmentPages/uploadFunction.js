0b5623caa7aca46d9ef0a3b11bc38030cb6719a7
DevFarzan <farzanhanif123@gmail.com>
2019-03-11 19:27:27 +0500

    changing in all

--- client/src/components/entertainment/entertainmentPages/uploadFunction.js
+++ client/src/components/entertainment/entertainmentPages/uploadFunction.js
@@ -34,38 +34,59 @@ class UploadFunction extends Component{
 
   render(){
     return(
-      <div className="row">
-        <div className="col-md-8">
-          <div className="uploadingcard">
-            <div>
-            <h4 style={{textAlign:"center"}}>Upload Your Video</h4>
-              <span className="addvalue">
-                <i class="fa fa-plus-square-o" style={{fontSize:"100px"}}></i>
-              </span>
-              <span className="modalform" style={{marginTop:"100px"}}>
 
-              <Button type="primary" onClick={this.showModal}>
-                Upload Video
-                  </Button>
-                {/*Modal*/}
-                <Modal
-                  title="Upload Video"
-                  visible={this.state.visible}
-                  onOk={this.handleOk}
-                  onCancel={this.handleCancel}
-                >
-                  <UploadForm/>
-              </Modal>
+          <div className="row" style={{border:'1px solid #80808033',width:'94%',marginLeft:'28px'}}>
+            <div className="col-md-6">
+              <h2 style={{fontSize:'40px',marginTop:'58px'}}><b>Upload and Share your videos</b></h2>
+              <p style={{marginTop:'2px'}}>videos not longer than 15 minutes
+By submitting your videos to Pakjazba, you acknowledge that you agree to pakjazba Terms of Service
+ and Community Guidelines.</p>
+ <Button type="primary" onClick={this.showModal}>
+   Upload Your Video
+ </Button>
 
-              </span>
+   <Modal
+     title="Upload Video"
+     visible={this.state.visible}
+     onOk={this.handleOk}
+     onCancel={this.handleCancel}
+   >
+     <UploadForm/>
+ </Modal>
+            </div>
+
+            <div className="col-md-6">
+              <img src="../images/ic.png" style={{width:'70%'}} />
             </div>
           </div>
-        </div>
-        <div className="col-md-4">
-        </div>
-      </div>
     )
   }
 }
 
 export default UploadFunction;
+
+
+{/*<div className="uploadingcard">
+  <div>
+  <h4 style={{textAlign:"center"}}>Upload Your Video</h4>
+    <span className="addvalue">
+      <i class="fa fa-plus-square-o" style={{fontSize:"100px"}}></i>
+    </span>
+    <span className="modalform" style={{marginTop:"100px"}}>
+
+    <Button type="primary" onClick={this.showModal}>
+      Upload Video
+        </Button>
+      {/*Modal
+      <Modal
+        title="Upload Video"
+        visible={this.state.visible}
+        onOk={this.handleOk}
+        onCancel={this.handleCancel}
+      >
+        <UploadForm/>
+    </Modal>
+
+    </span>
+  </div>
+</div>*/}
